d41da090c014fc4f62e24dd027febd329ca7c691
Lucas Pires <luc_50@outlook.com>
2020-03-21 23:09:26 -0300

    Adiconado a validacao dos campos e exibicao das mensagems de retonor

--- script.php
+++ script.php
@@ -1,10 +1,18 @@
 <?php
+
 /**
  * Created by PhpStrom.
  * User: Lucas
  * Date: 31/01/2020
  * Time: 21:32
  */
+/* Documentaação
+ * 
+ * session_start() cria uma sessão ou resume a sessão atual baseado em um id de sessão passado via GET ou POST, ou passado via cookie.
+ * 
+ * Vai inicializar a sessão php
+ */
+session_start();
 
 $categorias = [];
 $categorias[] = 'infantil';
@@ -13,60 +21,60 @@ $categorias[] = 'adulto';
 $categorias[] = 'idoso';
 
 //print_r($categorias);
-
 //esse arquivo vai recuperar qualquer informação que vier para o post
 $nome = $_POST['nome'];
 $idade = $_POST['idade'];
 
-//Validação dos dados
-//"empty vai validar se uma determina string está com valor ou não (vazio)"
-if(empty($nome))
-{
-    echo 'O campo do nome não pode estar vazio';
+/* Documentação
+ * 
+ * Validação dos dados
+ * 
+ * "empty vai validar se uma determina string está com valor ou não (vazio)"
+ * 
+ * "strlen" - vai contar a quantidado de caracteres
+ * 
+ * "is_numeric" - ira validadar se a variavel é um número
+ */
+if (empty($nome)) {
+    $_SESSION['menssagem.de.erro'] = 'O campo nome não pode estar vazio';
+    header($string = 'location: index.php');
     return;
-}
-//"strlen" - vai contar a quantidado de caracteres
-if(strlen($nome) < 3)
-{
-   echo'O nome deve contar mais de 3 caracteres';
-   return;
-}
-if(strlen($nome) > 40)
-{
-    echo'O nome não pode ter mais de 40 caracteres';
+} else if (strlen($nome) < 3) {
+    $_SESSION['menssagem.de.erro'] = 'O nome deve contar mais de 3 caracteres';
+    header($string = 'location: index.php');
     return;
-}
-//"is_numeric" - ira validadar se a variavel é um número
-if(!is_numeric($idade))
-{
-    echo'Informe um número para a idade';
+} else if (strlen($nome) > 40) {
+    $_SESSION['menssagem.de.erro'] = 'O nome não pode ter mais de 40 caracteres';
+    header($string = 'location: index.php');
+    return;
+} else if (!is_numeric($idade)) {
+    $_SESSION['menssagem.de.erro'] = 'Informe um número para a idade';
+    header($string = 'location: index.php');
     return;
 }
 
-if($idade >= 6 && $idade < 12)
-{
-    for($i = 0; $i <= count($categorias); $i++)
-    {
-        if ($categorias[$i] == 'infantil'){
-            echo "O nadador " .$nome. ", compete na categoria " .$categorias[$i] .".";
+if ($idade >= 6 && $idade < 12) {
+    for ($i = 0; $i <= count($categorias); $i++) {
+        if ($categorias[$i] == 'infantil') {
+            $_SESSION['menssagem.de.sucesso'] = "O nadador " . $nome . ", compete na categoria " . $categorias[$i] . ".";
+            header($string = 'location: index.php');
+            return;
         }
     }
-}
-else if ($idade >= 13 && $idade <= 18)
-{
-    for($i = 0; $i <= count($categorias); $i++)
-    {
-        if ($categorias[$i] == 'adolecente'){
-            echo "O nadador " .$nome. ", compete na categoria " .$categorias[$i] .".";
+} else if ($idade >= 13 && $idade <= 18) {
+    for ($i = 0; $i <= count($categorias); $i++) {
+        if ($categorias[$i] == 'adolecente') {
+            $_SESSION['menssagem.de.sucesso'] = "O nadador " . $nome . ", compete na categoria " . $categorias[$i] . ".";
+            header($string = 'location: index.php');
+            return;
         }
     }
-}
-else{
-    for ($i = 0; $i <= count($categorias); $i++)
-    {
-        if ($categorias[$i] == 'adulto'){
-            echo "O nadador " .$nome. ", compete na categoria " .$categorias[$i] .".";
+} else {
+    for ($i = 0; $i <= count($categorias); $i++) {
+        if ($categorias[$i] == 'adulto') {
+            $_SESSION['menssagem.de.sucesso'] = "O nadador " . $nome . ", compete na categoria " . $categorias[$i] . ".";
+            header($string = 'location: index.php');
+            return;
         }
     }
-
 }
